9925f99ed41974b3bee363a7f38469f8cd3e52ad
Manuel Anderson <manuel.anderson@mercadolibre.com>
2023-08-21 12:32:27 -0300

    Added new models for testing

--- Price/predictor.py
+++ Price/predictor.py
@@ -9,9 +9,6 @@ data_file = sys.argv[1]
 with open(data_file) as json_file:
     data_json = json.load(json_file)
 
-print("La información inicial es la siguiente")
-print(data_json)
-
 # ---------------------------------- Construcción de diccionario base ---------------------------------------
 # Tenemos que de alguna forma pasar del json de entrada a las columnas que tiene el modelo
 dict_df = dict()
@@ -43,6 +40,9 @@ dict_df["ambience"] = 0
 dict_df["bedrooms"] = 0
 dict_df["bathrooms"] = 0
 dict_df["surface_total"] = 0
+dict_df["centros_comerciales_cercanos"] = 0
+dict_df["parques_cercanos"] = 0
+dict_df["escuelas_cercanas"] = 0
 
 
 # ---------------------------------- Rellenamos el diccionario base con info que nos llego ---------------------------------------
@@ -76,9 +76,31 @@ model = load('Models/gboost_model.joblib')
 df = pd.DataFrame({k: [v] for k, v in dict_df.items()})
 
 # Debemos ordenar las columnas de la misma forma que estan en el modelo
-df = df[['characteristincs_balcony', 'characteristincs_yard', 'characteristincs_rooftop', 'characteristincs_garage', 'amenities_amoblado', 'amenities_cancha_paddle', 'amenities_cancha_tenis', 'amenities_gimnasio', 'amenities_hidromasaje', 'amenities_laundry', 'amenities_microcine', 'amenities_parrilla', 'amenities_piscina', 'amenities_sala_de_juegos', 'amenities_sauna', 'amenities_solarium', 'amenities_spa', 'amenities_sum', 'amenities_centros_comerciales_cercanos', 'amenities_parques_cercanos', 'amenities_escuelas_cercanas', 'amenities_estacionamiento_visitas', 'antiquity', 'ambience', 'bedrooms', 'bathrooms', 'surface_total', 'location_neighbourhood_Abasto', 'location_neighbourhood_Agronomía', 'location_neighbourhood_Almagro', 'location_neighbourhood_Almagro Norte', 'location_neighbourhood_Almagro Sur', 'location_neighbourhood_Balvanera', 'location_neighbourhood_Congreso', 'location_neighbourhood_Once', 'location_neighbourhood_Barracas', 'location_neighbourhood_Barrio Norte', 'location_neighbourhood_La isla', 'location_neighbourhood_Parque las Heras', 'location_neighbourhood_Recoleta', 'location_neighbourhood_Belgrano', 'location_neighbourhood_Boedo', 'location_neighbourhood_Caballito', 'location_neighbourhood_Chacarita', 'location_neighbourhood_Coghlan', 'location_neighbourhood_Colegiales', 'location_neighbourhood_Constitución', 'location_neighbourhood_Flores', 'location_neighbourhood_Floresta', 'location_neighbourhood_La boca', 'location_neighbourhood_La paternal', 'location_neighbourhood_Liniers', 'location_neighbourhood_Mataderos', 'location_neighbourhood_Microcentro', 'location_neighbourhood_Monserrat', 'location_neighbourhood_Monte Castro', 'location_neighbourhood_Nuñez', 'location_neighbourhood_Palermo', 'location_neighbourhood_Parque Avellaneda', 'location_neighbourhood_Parque Centenario', 'location_neighbourhood_Parque Chacabuco', 'location_neighbourhood_Parque Chas', 'location_neighbourhood_Parque Patricios', 'location_neighbourhood_Pompeya', 'location_neighbourhood_Primera Junta', 'location_neighbourhood_Puerto Madero', 'location_neighbourhood_Retiro', 'location_neighbourhood_Saavedra', 'location_neighbourhood_San Cristobal', 'location_neighbourhood_San Nicolas', 'location_neighbourhood_San Telmo', 'location_neighbourhood_Velez Sarsfield', 'location_neighbourhood_Versalles', 'location_neighbourhood_Villa Crespo', 'location_neighbourhood_Villa del Parque', 'location_neighbourhood_Villa Devoto', 'location_neighbourhood_Villa General Mitre', 'location_neighbourhood_Villa Lugano', 'location_neighbourhood_Villa Luro', 'location_neighbourhood_Villa Ortuzar', 'location_neighbourhood_Villa Pueyrredon', 'location_neighbourhood_Villa Real', 'location_neighbourhood_Villa Riachuelo', 'location_neighbourhood_Villa Santa Rita', 'location_neighbourhood_Villa Soldati', 'location_neighbourhood_Villa Urquiza']]
+df = df[['characteristincs_balcony', 'characteristincs_yard', 'characteristincs_rooftop', 'characteristincs_garage', 'amenities_amoblado', 'amenities_cancha_paddle', 'amenities_cancha_tenis', 'amenities_gimnasio', 'amenities_hidromasaje', 'amenities_laundry', 'amenities_microcine', 'amenities_parrilla', 'amenities_piscina', 'amenities_sala_de_juegos', 'amenities_sauna', 'amenities_solarium', 'amenities_spa', 'amenities_sum', 'amenities_estacionamiento_visitas', 'antiquity', 'ambience', 'bedrooms', 'bathrooms', 'surface_total', 'centros_comerciales_cercanos', 'parques_cercanos', 'escuelas_cercanas', 'location_neighbourhood_Abasto', 'location_neighbourhood_Agronomía', 'location_neighbourhood_Almagro', 'location_neighbourhood_Almagro Norte', 'location_neighbourhood_Almagro Sur', 'location_neighbourhood_Balvanera', 'location_neighbourhood_Congreso', 'location_neighbourhood_Once', 'location_neighbourhood_Barracas', 'location_neighbourhood_Barrio Norte', 'location_neighbourhood_La isla', 'location_neighbourhood_Parque las Heras', 'location_neighbourhood_Recoleta', 'location_neighbourhood_Belgrano', 'location_neighbourhood_Belgrano Barrancas', 'location_neighbourhood_Belgrano C', 'location_neighbourhood_Belgrano La Imprenta', 'location_neighbourhood_Belgrano R', 'location_neighbourhood_Boedo', 'location_neighbourhood_Caballito', 'location_neighbourhood_Caballito Barrio Ingles', 'location_neighbourhood_Caballito Cid Campeador', 'location_neighbourhood_Caballito Norte', 'location_neighbourhood_Caballito Parque Rivadavia', 'location_neighbourhood_Caballito Primera Junta', 'location_neighbourhood_Caballito Sur', 'location_neighbourhood_Chacarita', 'location_neighbourhood_Coghlan', 'location_neighbourhood_Colegiales', 'location_neighbourhood_Constitución', 'location_neighbourhood_Flores', 'location_neighbourhood_Flores Norte', 'location_neighbourhood_Flores Sur', 'location_neighbourhood_Floresta', 'location_neighbourhood_Floresta Norte', 'location_neighbourhood_Floresta Sur', 'location_neighbourhood_La boca', 'location_neighbourhood_La paternal', 'location_neighbourhood_Liniers', 'location_neighbourhood_Mataderos', 'location_neighbourhood_Microcentro', 'location_neighbourhood_Monserrat', 'location_neighbourhood_Monte Castro', 'location_neighbourhood_Nuñez', 'location_neighbourhood_Nuñez Lomas', 'location_neighbourhood_Nuñez River', 'location_neighbourhood_Palermo', 'location_neighbourhood_Palermo Botánico', 'location_neighbourhood_Palermo Boulevard', 'location_neighbourhood_Palermo Chico', 'location_neighbourhood_Palermo Hollywood', 'location_neighbourhood_Palermo Las Cañitas', 'location_neighbourhood_Palermo Pacífico', 'location_neighbourhood_Palermo Plaza Italia', 'location_neighbourhood_Palermo Soho', 'location_neighbourhood_Palermo Viejo', 'location_neighbourhood_Parque Avellaneda', 'location_neighbourhood_Parque Centenario', 'location_neighbourhood_Parque Chacabuco', 'location_neighbourhood_Parque Chas', 'location_neighbourhood_Parque Patricios', 'location_neighbourhood_Pompeya', 'location_neighbourhood_Primera Junta', 'location_neighbourhood_Puerto Madero', 'location_neighbourhood_Retiro', 'location_neighbourhood_Retiro Catalinas', 'location_neighbourhood_Retiro Plaza San Martín', 'location_neighbourhood_Saavedra', 'location_neighbourhood_San Cristobal', 'location_neighbourhood_San Nicolas', 'location_neighbourhood_Tribunales', 'location_neighbourhood_San Telmo', 'location_neighbourhood_Velez Sarsfield', 'location_neighbourhood_Versalles', 'location_neighbourhood_Villa Crespo', 'location_neighbourhood_Villa del Parque', 'location_neighbourhood_Villa Devoto', 'location_neighbourhood_Villa General Mitre', 'location_neighbourhood_Villa Lugano', 'location_neighbourhood_Villa Luro', 'location_neighbourhood_Villa Ortuzar', 'location_neighbourhood_Villa Pueyrredon', 'location_neighbourhood_Villa Real', 'location_neighbourhood_Villa Riachuelo', 'location_neighbourhood_Villa Santa Rita', 'location_neighbourhood_Villa Soldati', 'location_neighbourhood_Villa Urquiza']]
 
 prediction = model.predict(df)
 
 print("Resultado Prediccion: ")
 print(prediction)
+
+# ---------------------------------- Busqueda de similares ---------------------------------------
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+df_similarity = pd.read_csv("source.csv")
+df_similarity.drop(["Unnamed: 0"], axis=1, inplace=True)
+#df_similarity = df_similarity.loc[df_similarity.price > 100]
+#df_similarity = df_similarity.loc[df_similarity.surface_total < 400]
+
+new_row = df.to_dict("records")[0]
+new_row["price"] = prediction[0]
+
+similarity_scores = cosine_similarity(df_similarity.values, [list(new_row.values())])
+df_similarity['Similarity'] = similarity_scores
+df_similarity = df_similarity.sort_values(by='Similarity', ascending=False)
+
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
+
+#top_rows = df_similarity.head(3).to_dict("records")
+df_similarity.head(3).to_csv("Output/similar_properties-"+timestr+".csv")
